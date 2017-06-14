2f66056f259f9c453f71b2032ac031e77c22dd71
SergeevAI <ai.sergeev89@gmail.com>
2017-06-14 10:34:24 +0300

    CBV

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-from .views import index_page, post_detail
+from .views import IndexView, post_detail
 
 urlpatterns = [
-    url(r'^$', index_page, name='index'),
+    url(r'^$', IndexView.as_view(), name='index'),
     url(r'article/(?P<id>[0-9]+)/$', post_detail, name='post_detail'),
 ]
 
