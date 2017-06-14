9f69169691f4b9152b3b8cead2baf847e39ac6fc
SergeevAI <ai.sergeev89@gmail.com>
2017-06-14 11:01:10 +0300

    conflicts

--- src/users_list/views.py
+++ src/users_list/views.py
@@ -1,11 +1,7 @@
 from django.shortcuts import render
-<<<<<<< HEAD
 from django.views.generic import ListView, DetailView
-=======
-from django.views.generic import ListView
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
->>>>>>> 2f66056f259f9c453f71b2032ac031e77c22dd71
 from django.contrib.auth.models import User
 
 @method_decorator(login_required, name='dispatch')
@@ -13,16 +9,10 @@ class IndexView(ListView):
     template_name = 'users_list/users_list.html'
     model = User
     context_object_name = "users"
-<<<<<<< HEAD
 
 
+@method_decorator(login_required, name='dispatch')
 class UserDetailView(DetailView):
     template_name = 'users_list/user_detail.html'
     model = User
     context_object_name = "user"
-
-
-
-# Create your views here.
-=======
->>>>>>> 2f66056f259f9c453f71b2032ac031e77c22dd71
